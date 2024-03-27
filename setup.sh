491b6f9236a34e5460dffb4d7fe99d60b68a9314
David <david.cosc@gmx.net>
2024-03-27 20:05:43 +0100

    feat: added setup script for dev env

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+echo "Update and upgrade deps ......"
+apt update && apt upgrade -y
+echo "Update and upgrade deps ..[ok]"
+echo "Install python3 ......"
+apt install python3 -y
+python3 --version
+echo "Install python3 ..[ok]"
+echo "Install pip3 ......"
+apt install python3-pip -y
+pip3 --version
+echo "Install pip3 ..[ok]"
+echo "Install python3 pwntools ......"
+pip3 install pwntools
+echo "Install python3 pwntools ..[ok]"
+echo "Install patchelf ......"
+apt install patchelf -y
+echo "Install patchelf ..[ok]"
+echo "Install pwninit ......"
+FILE="./pwninit"
+if test -f "${FILE}"; then
+  echo "Install pwninit ..[ok]"
+else
+  wget "https://github.com/io12/pwninit/releases/download/3.3.1/pwninit"
+  chmod +x "./pwninit"
+  echo "Install pwninit ..[ok]"
+fi
+echo "Install git ......"
+apt install git -y
+echo "Install git ..[ok]"
+echo "Install pwndbg ......"
+FILE="./pwndbg"
+if test -d "${FILE}"; then
+  echo "Install pwndbg ..[ok]"
+else
+  git clone https://github.com/pwndbg/pwndbg
+  cd pwndbg
+  chmod +x ./setup.sh
+  ./setup.sh
+  cd ..
+  echo "Install pwndbg ..[ok]"
+fi
