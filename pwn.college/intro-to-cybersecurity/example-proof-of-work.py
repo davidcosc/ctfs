d275f1ef822c6bb86034306df7e3c262f70b4336
David Cornel <davidcornel@bwpm-dxx1v2mx61.fritz.box>
2024-05-31 20:36:32 +0200

    chore: refactor proof of work

--- pwn.college/intro-to-cybersecurity/example-proof-of-work.py
+++ pwn.college/intro-to-cybersecurity/example-proof-of-work.py
@@ -1,10 +1,11 @@
 import base64
+import os
 import re
-import subprocess
 from Crypto.Hash.SHA256 import SHA256Hash
 
+
 """
-difficulty = 2
+    difficulty = 2
 
     challenge = get_random_bytes(32)
     show_b64("challenge", challenge)
@@ -12,28 +13,53 @@ difficulty = 2
     response = input_b64("response")
     if SHA256Hash(challenge + response).digest()[:difficulty] == (b'\0' * difficulty):
         show("flag", flag.decode())
-
 """
 
-pat0 = re.compile(r".*challenge \(b64\): (.*)\n")
-proc = subprocess.Popen(["/challenge/run"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-i = 0
-challenge = None
+
+prefix = b"\0" * 2
+pat0 = re.compile(r".*challenge \(b64\): (.*)")
+r_pipe0, w_pipe0 = os.pipe()
+r_pipe1, w_pipe1 = os.pipe()
+
+pid = os.fork()
+
+if pid == -1:
+	print("Erro fork.")
+	exit(1)
+
+if pid == 0:
+	os.close(w_pipe0)
+	os.close(r_pipe1)
+
+	os.dup2(r_pipe0, 0)
+	os.close(r_pipe0)
+
+	os.dup2(w_pipe1, 1)
+	os.close(w_pipe1)
+
+	os.execv("/challenge/run", ["/challenge/run"])
+	print("Error execv.")
+	exit(1)
+
+os.close(r_pipe0)
+os.close(w_pipe1)
+
+output = os.fdopen(r_pipe1, "r")
+input = os.fdopen(w_pipe0, "wb")
 
 while True:
-	line = proc.stdout.readline().decode("utf-8")
+	line = output.readline()
 	print(line, end="")
 	m = pat0.match(line)
 	if m:
-		prefix = b"\0\0"
 		challenge = base64.standard_b64decode(m.group(1))
+		print(f"Got challenge {challenge}")
+		i = 0
 		while True:
-			response = challenge + str(i).encode("utf-8")
-			resp_sha = SHA256Hash(response).digest()[:2]
-			print(f"Response {response} sha digest[:2] {resp_sha} should equal {prefix}.")
-			if resp_sha == prefix:
-				response = base64.standard_b64encode(str(i).encode("utf-8"))
-				proc.stdin.write(response + b"\n")
-				proc.stdin.flush()
+			sha256 = SHA256Hash(challenge + str(i).encode("utf-8")).digest()
+			print(f"Compare i {i} sha256[:2] {sha256[:2]} to {prefix}")
+			if sha256[:2] == prefix:
+				input.write(base64.standard_b64encode(str(i).encode("utf-8")) + b"\n")
+				input.flush()
 				break
-			i = i + 1
+			i += 1
